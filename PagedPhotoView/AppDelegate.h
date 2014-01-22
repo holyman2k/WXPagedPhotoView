d8b96f675e7b3080ac4c32acf08328462c5163fd
Charlie Wu <c.wu@webfm.net>
2014-01-22 11:56:19 +1100

    Initial Commit

---
+++ PagedPhotoView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PagedPhotoView
+//
+//  Created by Charlie Wu on 22/01/2014.
+//  Copyright (c) 2014 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PagedPhotoViewTests/PagedPhotoViewTests.m
@@ -0,0 +1,34 @@
+//
+//  PagedPhotoViewTests.m
+//  PagedPhotoViewTests
+//
+//  Created by Charlie Wu on 22/01/2014.
+//  Copyright (c) 2014 Charlie Wu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PagedPhotoViewTests : XCTestCase
+
+@end
+
+@implementation PagedPhotoViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
